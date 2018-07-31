eb86feb2512cc9552c08920a83590af20e02c34b
ali <ali-mamaev2000@mail.ru>
2018-07-31 22:28:30 +0300

    'lesson7'

---
+++ api/routes/orders.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/', (req, res, next) => {
+	res.status(200).json({
+		message: 'Orders were fetched'
+	});
+});
+
+router.post('/', (req, res, next) => {
+	const order = {
+		productId: req.body.productId,
+		quantity: req.body.quantity
+	}
+	res.status(201).json({
+		message: 'Order was created',
+		order
+	});
+});
+
+router.get('/:id', (req, res, next) => {
+	res.status(201).json({
+		message: 'Order details',
+		id: req.params.id
+	});
+});
+
+router.delete('/:id', (req, res, next) => {
+	res.status(201).json({
+		message: 'Order deleted',
+		id: req.params.id
+	});
+});
+
+module.exports = router;
